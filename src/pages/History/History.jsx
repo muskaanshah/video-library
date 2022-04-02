c8fd106d75f4564a6087efc28581cea57f19362e
muskaanshah <hmuskaan11@gmail.com>
2022-04-02 17:47:41 +0530

    feat: Horizontal card

---
+++ src/pages/History/History.jsx
@@ -0,0 +1,13 @@
+import { VideoCardHorizontal } from "../../components/VideoCardHorizontal/VideoCardHorizontal";
+
+function History() {
+	return (
+		<div className="container-body color-white p-1">
+			<div className="videocard-display mt-1-5">
+				<VideoCardHorizontal />
+			</div>
+		</div>
+	);
+}
+
+export { History };
---
+++ src/pages/History/components/HistoryVideoModal.jsx
@@ -0,0 +1,24 @@
+function HistoryVideoModal() {
+	return (
+		<div className="bg-grey-dark videocard-modal py-0-5">
+			<p className="videocard-modal-action my-0">
+				<span className="material-icons-outlined">delete</span>
+				<span>REMOVE FROM HISTORY</span>
+			</p>
+			<p className="videocard-modal-action my-0">
+				<span className="material-icons-outlined">watch_later</span>
+				<span>WATCH LATER</span>
+			</p>
+			<p className="videocard-modal-action my-0">
+				<span className="material-icons-outlined">playlist_add</span>
+				<span>SAVE TO PLAYLIST</span>
+			</p>
+			<p className="videocard-modal-action my-0">
+				<span className="material-icons-outlined">share</span>
+				<span>SHARE</span>
+			</p>
+		</div>
+	);
+}
+
+export { HistoryVideoModal };

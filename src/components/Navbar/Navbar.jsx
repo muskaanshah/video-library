2a15de9a685509b554db8fbd342ea0a8a624d58f
muskaanshah <hmuskaan11@gmail.com>
2022-04-19 23:48:02 +0530

    fix: warning

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useAuth, useTheme } from "../../context";
+import { useTheme } from "../../context";
 import { Drawer } from "../Drawer/Drawer";
 import { avatar } from "../../assets";
 import "./navbar.css";

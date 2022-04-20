d547b715bf1308407b26c496d7f14bffa204acdf
muskaanshah <hmuskaan11@gmail.com>
2022-04-21 04:17:39 +0530

    feat: no videos div shown

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -9,9 +9,15 @@ function Explore() {
         <div className="container-body color-white p-1">
             <FilterSection />
             <div className="videocard-display mt-1-5">
-                {videoState.videos.map((video) => (
-                    <VideoCard key={video._id} video={video} />
-                ))}
+                {videoState.videos.length > 0 ? (
+                    videoState.videos.map((video) => (
+                        <VideoCard key={video._id} video={video} />
+                    ))
+                ) : (
+                    <div className="centered width-100">
+                        <h3>No videos to show for the selected categories</h3>
+                    </div>
+                )}
             </div>
         </div>
     );

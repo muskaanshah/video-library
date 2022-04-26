a99579485dd4868d4744960316d650fd72726060
muskaanshah <hmuskaan11@gmail.com>
2022-04-26 15:05:32 +0530

    style: minor changes

--- src/pages/NotFound/PageNotFound.jsx
+++ src/pages/NotFound/PageNotFound.jsx
@@ -6,8 +6,10 @@ function PageNotFound() {
     return (
         <div className="container-body centered flex-column">
             <img src={error} alt="404error" className="img-pagenotfound" />
-            <h2 className="color-white">404 error: Page not found</h2>
-            <Link to="/" className="btn bg-primary color-black borderradius-0-5">
+            <Link
+                to="/"
+                className="btn bg-primary color-black borderradius-0-5 fw-500 p-1 fs-0-9"
+            >
                 Go to Home
             </Link>
         </div>
